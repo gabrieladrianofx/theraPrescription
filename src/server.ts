6f81af67bf82667a55430660bdc442cd516c3378
GabrielAdriano99 <gabrieladrianofx@gmail.com>
2022-04-19 01:21:24 -0300

    🚚  Definition of request routes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,14 @@
 import express from "express";
+import PatientControllers from "./controllers/PatientControllers";
+import bodyParser from "body-parser";
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/patient', PatientControllers.index);
+app.post('/patient', PatientControllers.create);
+app.put('/patient', PatientControllers.update);
+app.delete('/patient', PatientControllers.delete);
 
 app.listen(3000, () => console.log("Server is running"));
