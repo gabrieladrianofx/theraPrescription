b35d8be3746c833e03eaa1a88cd1103b12be0a07
GabrielAdriano99 <gabrieladrianofx@gmail.com>
2022-05-01 02:29:40 -0300

    📈 creating repository for access to database data queries

--- src/repositories/PatientRepository.ts
+++ src/repositories/PatientRepository.ts
@@ -7,7 +7,7 @@ class PatientRepository {
         return await knex.select().from('Patient')
     }
 
-    async getByField(name, value) {
+    async getFieldBy(name, value) {
         const result = await knex.select().from('Patient').where(name, value)
         return result.length > 0 ? result[0] : null
     }
@@ -15,20 +15,20 @@ class PatientRepository {
     async addPatient(patient) {
         try{
 
-            if (await this.getByField('cpf', patient.cpf)) {
+            if (await this.getFieldBy('cpf', patient.cpf)) {
                 return { error: true, message: 'CPF already exists. Try another.' }
             } 
-            if (await this.getByField('email', patient.email)) {
+            if (await this.getFieldBy('email', patient.email)) {
                 return { error: true, message: 'E-mail already exists. Try another.' }
             }
 
             await knex('Patient').insert({
                 cpf: patient.cpf,
                 name: patient.name,
-                birthDay: patient.birthDay,
                 sex: patient.sex,
                 email: patient.email,
                 phone: patient.phone,
+                birthDay: patient.birthDay,
                 address: patient.address,
                 district: patient.district,
                 city: patient.city,
@@ -42,31 +42,55 @@ class PatientRepository {
         }
     }
 
-    async change(patient) {
+    async change(patient, params) {
+        try {
+            const params_id = await this.getFieldBy('cpf', params)
+            
+            if(!params_id){
+                return { error: true, message: 'data not found in database' }
+            }
 
-        if(await this.getByField('cpf', patient.cpf)){
+            // if(await this.getFieldBy('email', patient.email))
+            
+            // console.log(params_id.id)
             await knex('Patient').update({
                 cpf: patient.cpf,
                 name: patient.name,
-                birthDay: patient.birthDay,
                 sex: patient.sex,
                 email: patient.email,
                 phone: patient.phone,
+                birthDay: patient.birthDay,
                 address: patient.address,
                 district: patient.district,
                 city: patient.city,
                 state: patient.state,
                 zip_code: patient.zip_code
-            })
+            }).where({id: params_id.id})
+            
             return {...patient}
+        } catch(e) {
+            return new InternalError()
         }
-        return 
-    }
-
-    async remove(patient) {
         
     }
 
+    async remove(patient) {
+        try {
+
+            const params_id = await this.getFieldBy('cpf', patient)
+
+            if(!params_id) {
+                return { error: true, message: 'data not found in database' }
+            }
+
+            await knex('Patient').where({id: params_id.id}).del()
+
+            return {cpf: params_id.cpf}
+
+        } catch(e) {
+            return new InternalError()
+        }
+    }
 }
 
 export default PatientRepository
